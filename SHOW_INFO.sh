5d4edbc14e1967f3538888f6d5d0bbc2a842b5cd
oai <oai@R740.OpenWrt.jonathan61.giize.com>
2023-08-13 17:18:41 +0800

    first commit

---
+++ SHOW_INFO.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo -e "\n=== Red Hat Release: ==="
+cat /etc/redhat-release
+
+echo -e "\n=== Kernel Information: ==="
+uname -a
+
+echo -e "\n=== CPU Information: ==="
+cat /proc/cpuinfo | grep "model name" | awk -F': ' '{print $2}' | head -1
+
+echo -e "\n=== PCI Devices: (X710) ==="
+lspci | grep "Intel Corporation Ethernet Controller X710"
+
+echo -e "\n"
