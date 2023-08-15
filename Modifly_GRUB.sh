555c5bd2bf0e732486bd9cd065ca295e6fc3285d
dong881 <joe888joejoe@gmail.com>
2023-08-15 13:23:50 +0800

    初版完成

---
+++ Modifly_GRUB.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+
+echo "--> Setting new kernel parameters:"
+
+# 設定要修改的參數
+# PCI parameters
+PCI="pci=assign-busses pci=realloc igb.max_vfs=2" 
+
+# IOMMU parameters
+IOMMU="intel_iommu=on iommu=pt"
+
+# CPU parameters  
+CPU="intel_pstate=disable nosoftlockup tsc=nowatchdog mitigations=off"
+
+# Memory parameters
+MEM="cgroup_memory=1 cgroup_enable=memory mce=off" 
+
+# Idle parameters
+IDLE="idle=poll"
+
+# Hugepages parameters  
+HPAGES="hugepagesz=1G hugepages=40 hugepagesz=2M hugepages=0 default_hugepagesz=1G"
+
+# Security parameters
+SECURITY="selinux=0 enforcing=0 audit=0" 
+
+# Watchdog parameters
+WATCHDOG="nmi_watchdog=0 softlockup_panic=0"
+
+# Scheduling parameters
+SCHED="skew_tick=1 isolcpus=managed_irq,domain,0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30 nohz_full=0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30 rcu_nocbs=0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30 rcu_nocb_poll"
+
+# Reconstruct parameter string
+NEW_PARAMS="$PCI $IOMMU $CPU $MEM $IDLE $HPAGES $SECURITY $WATCHDOG $SCHED"
+
+echo $NEW_PARAMS
+
+
+echo -e "\n--> Updating /etc/default/grub with new parameters..."
+
+# 修改 /etc/default/grub 檔案
+sudo sed -i "s/^GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX=\"$NEW_PARAMS\"/" /etc/default/grub
+
+echo "success"
+
+sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+echo -e "\nGRUB configuration updated successfully. Remember to manually reboot the system with 'sudo reboot' for changes to take effect.\n"
